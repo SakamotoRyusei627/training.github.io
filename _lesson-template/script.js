f385d1785a2cad6bb3b592c84e34bd20b134d938
坂本龍征 <116774180+SakamotoRyusei627@users.noreply.github.com>
2023-04-04 09:26:39 +0900

    Add files via upload

---
+++ _lesson-template/script.js
@@ -0,0 +1,13 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+
+// function multiplierArray(array){
+//   const result = [];
+//   for(const number of array){
+    
+//     result.push(number ** 2);
+//   }
+//   return result;
+// }
+// console.log(doubleArray([2,3,4]))
